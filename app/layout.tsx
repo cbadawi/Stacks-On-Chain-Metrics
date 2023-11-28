2520793f62f748cd23ef8f460bc6647f12b0ac64
cbadawi <cb_1974@protonmail.com>
2023-11-29 02:19:08 +0700

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Navbar from './components/navigation/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 // TODO improve SEO
@@ -17,7 +18,13 @@ export default function RootLayout({
   // TODO add headers, footers, navbars and sidebars here
   return (
     <html lang='en' className='bg-slate-900'>
-      <body className={inter.className}>{children}</body>
+      <head>
+        <link rel='icon' href='/favicon.ico' />
+      </head>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
