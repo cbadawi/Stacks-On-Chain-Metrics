886301595ca05dec56d41c3f300cf356188cf5f8
cbadawi <cb_1974@protonmail.com>
2023-12-07 16:07:54 +0700

    add placeholder about and contact pages

---
+++ app/about/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const About = () => {
+  // TODO mention the API, pointing to its documentation and offerings
+  return (
+    <text>
+      At Stacks Metrics, we believe in the power of blockchain technology and
+      its potential to revolutionize industries. Our mission is to make this
+      technology accessible and understandable for all. We provide a platform
+      with comprehensive blockchain analytics services, offering in-depth
+      insights, transactions, events, token transfers and much more. Our
+      platform is designed to deliver real-time and custom reports based on
+      real-time smart contract usage. Whether youre a business looking to make
+      informed decisions, or an individual looking to stay ahead of the curve,
+      our platform has everything you need. With a user-friendly interface and a
+      commitment to delivering quality results, were dedicated to making
+      blockchain accessible for everyone. Discover the hidden potential of
+      blockchain. Check out ourDashboardsand get started now!
+    </text>
+  );
+};
+
+export default About;
