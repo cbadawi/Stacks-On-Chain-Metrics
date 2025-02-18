d0fc7e2c644beddd0270730aa4efcc3f7857ce58
carl <cb_1974@protonmail.com>
2025-02-18 12:20:17 +0100

    avoid pk tampering

--- app/lib/auth/sessions/verifySession.ts
+++ app/lib/auth/sessions/verifySession.ts
@@ -2,6 +2,7 @@
 
 import { cookies } from 'next/headers';
 import { decryptSession } from './session';
+import { getAddressFromPublicKey } from '@stacks/transactions';
 
 /**
  * Reads the session cookie and verifies it.
@@ -14,7 +15,16 @@ export async function verifySession() {
     return null;
   }
   const session = await decryptSession(token!);
-  if (!session || !session.userId || !session.publicKey) {
+  // console.log('verifySession', {
+  //   session,
+  //   address: getAddressFromPublicKey(session?.publicKey || ''),
+  // });
+  if (
+    !session ||
+    !session.userId ||
+    !session.publicKey ||
+    session.userId !== getAddressFromPublicKey(session.publicKey)
+  ) {
     return null;
   }
   return session;
